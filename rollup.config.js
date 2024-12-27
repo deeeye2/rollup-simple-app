89d87509a76f2782d9462793b6bd3120bc7d4754
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-12-27 22:03:42 +0000

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
 import css from 'rollup-plugin-import-css';
 
 export default {
-  input: 'src/index.js', // Entry point
-  output: {
-    dir: 'dist', // Output directory
-    format: 'esm', // ES module format
-    sourcemap: true, // Include sourcemap for debugging
-  },
-  plugins: [
-    css(), // Handle CSS imports
-  ],
+    input: 'src/index.js', // Ensure this path matches the location of your entry file
+    output: {
+        dir: 'dist',
+        format: 'esm',
+        sourcemap: true,
+    },
+    plugins: [
+        css(),
+    ],
 };
