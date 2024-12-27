be71975db0c3996e2832f463efea1652fd85b62f
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-12-27 22:29:02 +0000

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,14 @@
-import css from "rollup-plugin-import-css";
+import css from 'rollup-plugin-import-css';
 
 export default {
-  input: "src/index.js",
-  output: {
-    file: "dist/bundle.js",
-    format: "iife",
-    sourcemap: true,
-  },
-  plugins: [
-    css({ output: "bundle.css" }), // Output CSS to dist/bundle.css
-  ],
+    input: 'src/index.js', // Entry point for the application
+    output: {
+        file: 'dist/bundle.js', // Output JS file
+        format: 'iife', // Immediately Invoked Function Expression
+        sourcemap: true, // Enable source maps
+    },
+    plugins: [
+        css({ output: 'bundle.css' }), // Generate CSS bundle
+    ],
 };
+
